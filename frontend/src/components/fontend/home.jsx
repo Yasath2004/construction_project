e1f4d4a3003ccd0458ceac23286ffa0cd2205d32
Yasath2004 <yasathamasara2@gmail.com>
2025-12-27 22:25:10 +0530

    about: Update home.jsx and style.scss for About Section styling

--- frontend/src/components/fontend/home.jsx
+++ frontend/src/components/fontend/home.jsx
@@ -45,22 +45,45 @@ import aboutImg from '../../assets/images/about-us.jpg';
 
 
       {/* About Section */}
-      <section className='section-2'>
-        <div className='container'>
+      <section className='section-2 py-5'>
+        <div className='container py-5'>
           <div className='row'>
             <div className='col-md-6'>
-              <img src={aboutImg} alt="About Us" className='img-fluid rounded'/>
+              <img src={aboutImg} alt="About Us" className='w-100'/>
 
               </div>
-            </div>
+              <div className='col-md-6'>
+                <span>About Us</span>
+                <h2>We Build <br/>Your Dream Home</h2>
+                <p>At UrbanEdge Construction, we are dedicated to turning your dream home into a reality. With years of experience in the construction industry, our team of skilled professionals is committed to delivering high-quality craftsmanship and exceptional service.</p>
+
+                <p>Choose UrbanEdge Construction for your next project and experience the difference of working with a team that values integrity, quality, and customer satisfaction above all else.</p>
+              </div>
+          </div> 
         </div>
       </section>
-
-      
-
     </main>
     <footer>
+      <div className='container'>
+        <div className='row'>
 
+        <div className='col-md-3'>
+          <h2>urbanEdge</h2>
+          <p>© 2024 UrbanEdge Construction. All rights reserved.</p>
+        </div>
+
+        <div className='col-md-3'>
+        </div>
+
+        <div className='col-md-3'>
+        </div>
+
+        <div className='col-md-3'>
+        </div>
+
+
+       </div>
+    </div>
     </footer>
     </>
   
