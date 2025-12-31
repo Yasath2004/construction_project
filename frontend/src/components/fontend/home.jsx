aaacfd5eec42176403961a77115c95fa14af997e
Yasath2004 <yasathamasara2@gmail.com>
2025-12-31 20:30:22 +0530

    update our project section

--- frontend/src/components/fontend/home.jsx
+++ frontend/src/components/fontend/home.jsx
@@ -54,7 +54,134 @@ import serviceimg from '../../assets/images/construction1.jpg';
                   <h2>our construction services</h2>
                   <p>we are a team of experienced professionals dedicated to delivering high-quality construction services tailored to your specific needs.</p>
                   </div>
-                  <div className='row'>
+                  <div className='row pt-4'>
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
+                    
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </section>
+
+            {/* our projects */}
+                        <section className='section-3 bg-light py-5'>
+              <div className='container-fluid py-5'>
+                <div className='section-header text-center mb-5'>
+                  <span>our services</span>
+                  <h2>our construction services</h2>
+                  <p>we are a team of experienced professionals dedicated to delivering high-quality construction services tailored to your specific needs.</p>
+                  </div>
+                  <div className='row pt-4'>
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
+                    
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
+                    <div className='col-md-3 col-lg-3'>
+                      <div className='item'>
+                        <div className='service-image'>
+                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <div className='service-body'>
+                            <div className='service-title'>
+                              <h3>residential construction</h3>
+                              </div>
+                              <div className='service-content'>
+                                <p>We specialize in building custom homes, renovations, and additions that reflect your unique style and needs.</p>
+                              </div>
+                              <a href='#' className='btn btn-primary'>read more</a>
+                          </div>
+                          </div>
+                      </div>
+                    </div>
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
