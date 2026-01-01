2f86728e3f7cfb6ba4ff3c267fe99f547328f35c
Yasath2004 <yasathamasara2@gmail.com>
2026-01-01 13:57:42 +0530

    comment section and update home page

--- frontend/src/components/fontend/home.jsx
+++ frontend/src/components/fontend/home.jsx
@@ -2,55 +2,69 @@ import react from 'react';
 import aboutImg from '../../assets/images/about-us.jpg'; 
 import Header from '../common/header.jsx';
 import Footer from '../common/footer.jsx';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 import serviceimg from '../../assets/images/construction1.jpg';
+import service2img from '../../assets/images/construction5.jpg';
+import service3img from '../../assets/images/construction3.jpg';
+import service4img from '../../assets/images/construction4.jpg';
+
 import constructionimg from '../../assets/images/construction2.jpg';
+import construction1img from '../../assets/images/construction6.jpg';
+import construction2img from '../../assets/images/construction7.jpg';
+import construction3img from '../../assets/images/construction8.jpg';
+
 import Icon1 from '../../assets/images/icon-1.svg';
 import Icon2 from '../../assets/images/icon-2.svg';
 import Icon3 from '../../assets/images/icon-3.svg';
+
+import avatarimg from '../../assets/images/author-2.jpg';
 
  const Home = () => {
   return (
     <>
     <Header/>
     <main>
-      {/* Hero Section */}
-      <section className='section-1'>
-        <div className='hero d-flex align-items-center' >
-          <div className='container-fluid'>
-            <div className='text-center'>
-              <span>Welcome Amazing Constructions</span>
-              <h1>crafting with<br/>percision and excellence</h1>
-              <p>Building dreams, one brick at a time. <br/>Your vision, our expertise.</p>
-              <div className='mt-4'>
-              <a className='btn btn-primary'>contact now</a>
-              <a className='btn btn-secondary ms-2'>view projects</a>
+            {/* Hero Section */}
+            <section className='section-1'>
+                  <div className='hero d-flex align-items-center' >
+                    <div className='container-fluid'>
+                      <div className='text-center'>
+                        <span>Welcome Amazing Constructions</span>
+                        <h1>crafting with<br/>percision and excellence</h1>
+                        <p>Building dreams, one brick at a time. <br/>Your vision, our expertise.</p>
+                        <div className='mt-4'>
+                        <a className='btn btn-primary'>contact now</a>
+                        <a className='btn btn-secondary ms-2'>view projects</a>
+                        </div>
+                      </div>  
+                    </div>
+                  </div>
+            </section>
+
+
+            {/* About Section */}
+            <section className='section-2 py-5'>
+              <div className='container py-5'>
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <img src={aboutImg} alt="About Us" className='w-100'/>
+
+                    </div>
+                    <div className='col-md-6'>
+                      <span>About Us</span>
+                      <h2>We Build <br/>Your Dream Home</h2>
+                      <p>At UrbanEdge Construction, we are dedicated to turning your dream home into a reality. With years of experience in the construction industry, our team of skilled professionals is committed to delivering high-quality craftsmanship and exceptional service.</p>
+
+                      <p>Choose UrbanEdge Construction for your next project and experience the difference of working with a team that values integrity, quality, and customer satisfaction above all else.</p>
+                    </div>
+                </div> 
               </div>
-            </div>  
-          </div>
-        </div>
-      </section>
+            </section>
 
-
-      {/* About Section */}
-      <section className='section-2 py-5'>
-        <div className='container py-5'>
-          <div className='row'>
-            <div className='col-md-6'>
-              <img src={aboutImg} alt="About Us" className='w-100'/>
-
-              </div>
-              <div className='col-md-6'>
-                <span>About Us</span>
-                <h2>We Build <br/>Your Dream Home</h2>
-                <p>At UrbanEdge Construction, we are dedicated to turning your dream home into a reality. With years of experience in the construction industry, our team of skilled professionals is committed to delivering high-quality craftsmanship and exceptional service.</p>
-
-                <p>Choose UrbanEdge Construction for your next project and experience the difference of working with a team that values integrity, quality, and customer satisfaction above all else.</p>
-              </div>
-          </div> 
-        </div>
-      </section>
-
-      {/* our Services*/}
+            {/* our Services*/}
             <section className='section-3 bg-light py-5'>
               <div className='container-fluid py-5'>
                 <div className='section-header text-center mb-5'>
@@ -79,7 +93,7 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
-                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <img src={service2img} alt='Service 2' className='w-100'/>
                           <div className='service-body'>
                             <div className='service-title'>
                               <h3>residential construction</h3>
@@ -95,7 +109,7 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
-                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <img src={service3img} alt='Service 3' className='w-100'/>
                           <div className='service-body'>
                             <div className='service-title'>
                               <h3>residential construction</h3>
@@ -111,7 +125,7 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
-                          <img src={serviceimg} alt='Service 1' className='w-100'/>
+                          <img src={service4img} alt='Service 4' className='w-100'/>
                           <div className='service-body'>
                             <div className='service-title'>
                               <h3>residential construction</h3>
@@ -184,10 +198,8 @@ import Icon3 from '../../assets/images/icon-3.svg';
               </div>
             </section>
               
-
-
             {/* our projects */}
-                        <section className='section-3 bg-light py-5'>
+            <section className='section-3 bg-light py-5'>
               <div className='container-fluid py-5'>
                 <div className='section-header text-center mb-5'>
                   <span>our projects</span>
@@ -215,7 +227,7 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
-                          <img src={constructionimg} alt='Service 1' className='w-100'/>
+                          <img src={construction1img} alt='Service 1' className='w-100'/>
                           <div className='service-body'>
                             <div className='service-title'>
                               <h3>srilanka project</h3>
@@ -231,7 +243,7 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
-                          <img src={constructionimg} alt='Service 1' className='w-100'/>
+                          <img src={construction2img} alt='Service 1' className='w-100'/>
                           <div className='service-body'>
                             <div className='service-title'>
                               <h3>india project</h3>
@@ -247,7 +259,7 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     <div className='col-md-3 col-lg-3'>
                       <div className='item'>
                         <div className='service-image'>
-                          <img src={constructionimg} alt='Service 1' className='w-100'/>
+                          <img src={construction3img} alt='Service 1' className='w-100'/>
                           <div className='service-body'>
                             <div className='service-title'>
                               <h3>residential construction</h3>
@@ -262,6 +274,64 @@ import Icon3 from '../../assets/images/icon-3.svg';
                     </div>
                   </div>
               </div>
+            </section>
+            {/* section-5 */}
+            <section className='section-5 py-5'>
+              <div className='container py-5'>
+                <div className='section-header text-center mb-5'>
+                  <span>Testimonnials</span>
+                  <h2>what people are saying about us</h2>
+                  <p>we offer a diverse of array of construction services. spanning residential,commercial and industrial projects.</p>
+                  </div>
+                      <Swiper
+                      spaceBetween={50}
+                      slidesPerView={3}
+                      onSlideChange={() => console.log('slide change')}
+                      onSwiper={(swiper) => console.log(swiper)}
+                    >
+                      <SwiperSlide>
+                        <div className='card shadow border-0'>
+                          <div className='card-body p4'>
+                            <div className='rating'>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                            </svg>
+                            </div>
+                            <div className='content py-4'>
+                              <p>"Amazing Constructions transformed our vision into reality with their exceptional craftsmanship and attention to detail. Highly recommend!"</p>
+                            </div>
+                            <br/>
+                            <div className='d-flex meta'>
+                              <div>
+                                <img src={avatarimg} alt='client'width={50} />
+                              </div>
+                              <div className='ps-3'>
+                                <div className='name'>jhone deo</div>
+                                <div>ceo,hazar company</div>
+                              </div>
+                          </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>Slide 2</SwiperSlide>
+                      <SwiperSlide>Slide 3</SwiperSlide>
+                      <SwiperSlide>Slide 4</SwiperSlide>
+                      ...
+                                </Swiper>
+              </div>
+
             </section>
     </main>
     <Footer/>
